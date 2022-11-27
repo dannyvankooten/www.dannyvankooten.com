66b33e2bd291e44d4614acb084d4115386e5d25c
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-11-27 11:56:27 +0100

    simplify optimize-images.sh, can all be handled by imagemagick

--- scripts/optimize-images.sh
+++ scripts/optimize-images.sh
@@ -1,11 +1,8 @@
+#!/usr/bin/env bash
+
 set -e
 
 echo "Optimizing images"
-echo "Before: $(du -h public/media | tail -n1)"
-mogrify -sample '1024>' -quality 82 -strip public/**/**/*.jpg 
-for f in public/**/**/*.jpg; do
-	jpegtran -optimize -perfect -outfile "$f" $f
-done
-
-optipng -quiet public/**/**/*.png
-echo "After: $(du -h public/media | tail -n1)"
+echo "Before: $(du -bch public/**/**/*.{jpg,png} | tail -n1)"
+mogrify -sample '1024>' -quality 80 -strip public/**/**/*.{jpg,png}
+echo "After: $(du -bch public/**/**/*.{jpg,png} | tail -n1)"
