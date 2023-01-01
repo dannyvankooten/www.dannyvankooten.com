7461b08d4b542b8f810cf259a3a512256e0f983b
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-01-01 17:09:50 +0100

    move footer icons into stylesheet

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env sh 
 set -e
 
 echo "Building Zola site"
 zola build
 
 echo "Minify stylesheet"
-minify -o public/styles.css public/styles.css
+lightningcss --minify -o public/styles.css public/styles.css
 
 echo "Sending to remote"
 rsync -ru public/. www.dvk.co:/var/www/dannyvankooten.com --delete
