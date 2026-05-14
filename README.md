2006d489b0b3671f232a1c889aaddc171911fb5f
Danny van Kooten <hi@dvk.co>
2026-05-14 08:47:10 +0200

    add screenshot

---
+++ README.md
@@ -0,0 +1,5 @@
+# www.dannyvankooten.com
+
+WordPress site. Custom theme. 
+
+![Screenshot of the site](/wp-content/themes/dvk26/screenshot.png)
--- wp-content/themes/dvk26/404.php
+++ wp-content/themes/dvk26/404.php
@@ -2,7 +2,7 @@
 <div class="container">
     <?php dvk26_home_link(); ?>
     <h1>Page not found</h1>
-    <p>The requested URL <code><?php echo esc_html($_SERVER['REQUEST_URI']) ?></code> was not found on this site.</p>
+    <p>The requested URL <code><?php echo esc_html($_SERVER['REQUEST_URI'] ?? '') ?></code> was not found on this site.</p>
     <p><a href="/">&laquo; Back to homepage</a></p>
 </div>
 <?php get_footer(); ?>
