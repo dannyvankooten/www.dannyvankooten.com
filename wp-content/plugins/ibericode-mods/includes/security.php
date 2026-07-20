805378b6e423cd1a15d63f7cf2402e5a6554f084
Danny van Kooten <hi@dvk.co>
2026-07-20 08:02:54 +0200

    update ibericode mods

--- wp-content/plugins/ibericode-mods/includes/security.php
+++ wp-content/plugins/ibericode-mods/includes/security.php
@@ -6,26 +6,6 @@ defined('ABSPATH') or exit;
 // Disable XMLRPC
 add_filter('xmlrpc_enabled', '__return_false');
 
-// Do not allow access to WordPress REST API for non-logged-in users
-add_filter('rest_authentication_errors', static function ($result) {
-    if (is_wp_error($result)) {
-        return $result;
-    }
-
-    $request_path = urldecode((string) ($_SERVER['REQUEST_URI'] ?? '')); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash
-    $query_param = $_GET['rest_route'] ?? ''; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash
-
-    if (! is_user_logged_in() && (str_contains($request_path, '/wp-json/wp/v2/users') || $query_param === '/wp/v2/users')) {
-        return new WP_Error(
-            'rest_not_logged_in',
-            'You are not currently logged in.',
-            ['status' => 401]
-        );
-    }
-
-    return $result;
-});
-
 // Prevent user enumeration via ?author=1
 add_action('init', static function () {
     if (isset($_GET['author'])) {
@@ -34,19 +14,13 @@ add_action('init', static function () {
     }
 }, 1);
 
-
 // Explicitly allow automatic updater, even if DISALLOW_FILE_MODS is enabled
-add_filter(
-    'file_mod_allowed',
-    static function ($allow, $context) {
-        if ('automatic_updater' === $context) {
-            return true;
-        }
-        return $allow;
-    },
-    10,
-    2
-);
+add_filter('file_mod_allowed', static function ($allow, $context) {
+    if ('automatic_updater' === $context) {
+        return true;
+    }
+    return $allow;
+}, 10, 2);
 
 // Reject all login requests submitted within 2 seconds of loading the page
 add_action('login_footer', static function () {
