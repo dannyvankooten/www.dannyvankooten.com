d97a05577939fd3395f01779d9017221cb67f421
Danny van Kooten <hi@dvk.co>
2026-08-07 12:30:57 +0200

    move ibericode-mods into mu-plugins

---
+++ wp-content/mu-plugins/ibericode-mods.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: ibericode mods
+Description: a collection of commonly needed WordPresss modifications or enhancements
+Author: Danny van Kooten
+Author URI: https://www.dannyvankooten.com/
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Update URI: https://www.ibericode.com/
+*/
+
+require __DIR__ . '/ibericode-mods/ibericode-mods.php';
